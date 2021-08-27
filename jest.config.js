419484ebf09df05509a5f4745ba1daaea138fee6
Krystian Kruk <kkruk@sumologic.com>
2021-08-27 14:12:56 +0200

    Fixed running UTs

--- jest.config.js
+++ jest.config.js
@@ -2,8 +2,12 @@ const { pathsToModuleNameMapper } = require('ts-jest/utils');
 const { compilerOptions } = require('./tsconfig');
 
 module.exports = {
+  rootDir: './',
   preset: 'ts-jest',
   testEnvironment: 'jsdom',
+  testMatch: [
+    '<rootDir>/src/opentelemetry-export-timestamp-enrichment/index.spec.ts',
+  ],
   testPathIgnorePatterns: [
     '/node_modules/',
     'src/opentelemetry-js',
