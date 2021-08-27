fa733a0635a9c85222ed1c40c0d418452cb68edf
Krystian Kruk <kkruk@sumologic.com>
2021-08-27 18:05:28 +0200

    Fixed CD script

--- scripts/publish-to-cdn.js
+++ scripts/publish-to-cdn.js
@@ -1,5 +1,5 @@
 const AWS = require('aws-sdk');
-const fs = require('fs');
+const fs = require('fs/promises');
 const { version } = require('../package.json');
 
 const getFileNames = () => {
@@ -23,24 +23,23 @@ const filenames = getFileNames();
 const s3 = new AWS.S3({ apiVersion: '2006-03-01' });
 const cloudfront = new AWS.CloudFront({ apiVersion: '2020-05-31' });
 
-const fileStream = fs.createReadStream('./dist/index.js');
-fileStream.on('error', (error) => {
-  console.error(error);
-  process.exit(1);
-});
-
 const main = async () => {
+  const file = await fs.readFile('./dist/index.js');
+
   for (const filename of filenames) {
+    console.log(`Upload ${filename}`);
     await s3
       .upload({
         Bucket: process.env.RUM_S3_BUCKET,
         Key: filename,
-        Body: fileStream,
+        Body: file,
         CacheControl: `max-age=${MAX_AGE}`,
+        ContentType: 'application/javascript',
       })
       .promise();
   }
 
+  console.log('Invalidate files');
   await cloudfront
     .createInvalidation({
       DistributionId: process.env.RUM_CLOUDFRONT_DISTRIBUTION_ID,
