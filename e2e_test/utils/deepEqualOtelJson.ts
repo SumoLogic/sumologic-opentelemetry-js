c973f4c9dab922007f97513259d41f3aca07f9ab
Krystian Kruk <kkruk@sumologic.com>
2021-10-04 19:46:58 +0200

    Fixed e2e tests

--- e2e_test/utils/deepEqualOtelJson.ts
+++ e2e_test/utils/deepEqualOtelJson.ts
@@ -1,24 +1,59 @@
 const ANY_STRING_KEYS = new Set(['traceId', 'spanId', 'parentSpanId']);
+const ANY_STRING_PATHS = new Set([
+  'resourceSpans.instrumentationLibrarySpans.spans.attributes.http.user_agent.value.stringValue',
+]);
 const ANY_NUMBER_KEYS = new Set([
   'timeUnixNano',
   'startTimeUnixNano',
   'endTimeUnixNano',
-  'doubleValue',
-  'intValue',
+]);
+const ANY_NUMBER_PATHS = new Set([
+  'resourceSpans.resource.attributes.sumologic.telemetry.sdk.export_timestamp.value.doubleValue',
+  'resourceSpans.instrumentationLibrarySpans.spans.attributes.http.response_content_length.value.intValue',
 ]);
 
 export const deepEqualOtelJson = (
   resp1: any,
   resp2: any,
-  path: (string | number)[] = [],
+  path: string[] = [],
 ) => {
+  const lastPathElement = path[path.length - 1];
+  const pathAsString = path.join('.');
+
   const fail = () => {
     throw new Error(
       `${JSON.stringify(resp1)} is not equal ${JSON.stringify(
         resp2,
-      )} in ${path.join('.')}`,
+      )} in ${pathAsString}`,
     );
   };
+
+  if (lastPathElement) {
+    if (
+      ANY_STRING_KEYS.has(lastPathElement) ||
+      ANY_STRING_PATHS.has(pathAsString)
+    ) {
+      if (
+        typeof resp1 !== 'string' ||
+        typeof resp2 !== 'string' ||
+        !resp1 ||
+        !resp2
+      ) {
+        fail();
+      }
+      return;
+    }
+
+    if (
+      ANY_NUMBER_KEYS.has(lastPathElement) ||
+      ANY_NUMBER_PATHS.has(pathAsString)
+    ) {
+      if (!Number.isFinite(resp1) || !Number.isFinite(resp2)) {
+        fail();
+      }
+      return;
+    }
+  }
 
   if (typeof resp1 !== typeof resp2) {
     fail();
@@ -27,28 +62,19 @@ export const deepEqualOtelJson = (
       fail();
     }
     resp1.forEach((element, index) =>
-      deepEqualOtelJson(element, resp2[index], [...path, index]),
+      deepEqualOtelJson(element, resp2[index], path),
     );
   } else if (typeof resp1 === 'object' && resp1 != null && resp2 != null) {
+    const objectPath = resp1.key;
     Object.keys(resp1).forEach((key) => {
       const value1 = resp1[key];
       const value2 = resp2[key];
-      if (ANY_STRING_KEYS.has(key)) {
-        if (
-          typeof value1 !== 'string' ||
-          typeof value2 !== 'string' ||
-          !value1 ||
-          !value2
-        ) {
-          fail();
-        }
-      } else if (ANY_NUMBER_KEYS.has(key)) {
-        if (!Number.isFinite(value1) || !Number.isFinite(value2)) {
-          fail();
-        }
-      } else {
-        deepEqualOtelJson(value1, value2, [...path, key]);
-      }
+
+      deepEqualOtelJson(
+        value1,
+        value2,
+        [...path, objectPath, key].filter(Boolean),
+      );
     });
   } else if (resp1 !== resp2) {
     fail();
--- e2e_test/utils/server.ts
+++ e2e_test/utils/server.ts
@@ -2,33 +2,48 @@ import express from 'express';
 import path from 'path';
 import { Server } from 'http';
 
-const app = express();
+export const startServer = ({ port }: { port: number }) => {
+  const app = express();
 
-app.use(express.json());
-app.use(express.static(path.join(__dirname, '../static')));
+  app.use(express.json());
 
-app.get('/rum.js', (req, res) => {
-  res.sendFile(path.join(__dirname, '../../dist/browser.js'));
-});
+  app.get('/logo-delay.png', (req, res) => {
+    setTimeout(() => {
+      res.sendFile(path.join(__dirname, '../static/logo.png'));
+    }, 1000);
+  });
 
-let resolveTraces: (value: any) => void;
-let tracesPromise = new Promise<any>((resolve) => {
-  resolveTraces = resolve;
-});
+  app.use(
+    express.static(path.join(__dirname, '../static'), {
+      maxAge: 0,
+      lastModified: false,
+      etag: false,
+      cacheControl: false,
+    }),
+  );
 
-app.post('/traces', (req, res) => {
-  res.send('');
-  resolveTraces(req.body);
-});
+  app.get('/rum.js', (req, res) => {
+    res.sendFile(path.join(__dirname, '../../dist/browser.js'));
+  });
 
-app.get('/traces', async (req, res) => {
-  res.send(await tracesPromise);
-});
+  let resolveTraces: (value: any) => void;
+  let tracesPromise = new Promise<any>((resolve) => {
+    resolveTraces = resolve;
+  });
 
-export const startServer = ({ port }: { port: number }) =>
-  new Promise<Server>((resolve) => {
+  app.post('/traces', (req, res) => {
+    res.send('');
+    resolveTraces(req.body);
+  });
+
+  app.get('/traces', async (req, res) => {
+    res.send(await tracesPromise);
+  });
+
+  return new Promise<Server>((resolve) => {
     const server = app.listen(port, () => {
       console.log(`Example app listening at http://localhost:${port}`);
       resolve(server);
     });
   });
+};
