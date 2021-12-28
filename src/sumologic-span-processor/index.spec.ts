34d6b3f6cf58499afabf802f857379e720263192
Krystian Kruk <kkruk@sumologic.com>
2021-12-28 16:55:43 +0100

    Added unit for long tasks context discovery

--- src/sumologic-span-processor/index.spec.ts
+++ src/sumologic-span-processor/index.spec.ts
@@ -2,6 +2,7 @@ import {
   BasicTracerProvider,
   Span,
   SpanExporter,
+  Tracer,
 } from '@opentelemetry/sdk-trace-base';
 import { ROOT_CONTEXT, SpanKind, TraceFlags } from '@opentelemetry/api';
 import { SumoLogicSpanProcessor } from './index';
@@ -25,6 +26,7 @@ let lastUID = 0;
 const nextUID = () => String(lastUID++);
 
 describe('SumoLogicSpanProcessor', () => {
+  let tracer: Tracer;
   let span: Span;
   let spanProcessor: SumoLogicSpanProcessor;
   let superOnEnd: jest.Mock;
@@ -39,7 +41,7 @@ describe('SumoLogicSpanProcessor', () => {
         return Promise.resolve();
       },
     };
-    const tracer = new BasicTracerProvider().getTracer('default');
+    tracer = new BasicTracerProvider().getTracer('default');
     span = new Span(
       tracer,
       ROOT_CONTEXT,
@@ -134,11 +136,50 @@ describe('SumoLogicSpanProcessor', () => {
   });
 
   test('drops root spans from instrumentation-user-interaction when there is no children', () => {
-    (span.instrumentationLibrary as any).name =
-      '@opentelemetry/instrumentation-user-interaction';
+    (span as any).instrumentationLibrary = {
+      name: '@opentelemetry/instrumentation-user-interaction',
+      version: undefined,
+    };
     spanProcessor.onStart(span);
     spanProcessor.onEnd(span);
     jest.runAllTimers();
     expect(superOnEnd).not.toBeCalled();
+  });
+
+  test('finds context for longtask spans', async () => {
+    (span as any).instrumentationLibrary = {
+      name: '@opentelemetry/instrumentation-long-task',
+      version: undefined,
+    };
+    const parent = new Span(
+      tracer,
+      ROOT_CONTEXT,
+      'parent test span',
+      { spanId: nextUID(), traceId: nextUID(), traceFlags: TraceFlags.SAMPLED },
+      SpanKind.INTERNAL,
+    );
+
+    let resolvePromise: any;
+    const promise = new Promise((resolve) => {
+      resolvePromise = resolve;
+    });
+
+    superOnEnd.mockImplementation((spanToEnd) => {
+      if (spanToEnd === parent) {
+        resolvePromise();
+      }
+    });
+
+    spanProcessor.onStart(span);
+    spanProcessor.onStart(parent);
+    span.end();
+    spanProcessor.onEnd(span);
+    parent.end();
+    spanProcessor.onEnd(parent);
+
+    await promise;
+
+    expect(span.parentSpanId).toBe(parent.spanContext().spanId);
+    expect(span.spanContext().traceId).toBe(parent.spanContext().traceId);
   });
 });
