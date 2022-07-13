dc8a0dbae1bc507f2dfa7db5cfc812ff68e117fb
Krystian Kruk <kkruk@sumologic.com>
2022-07-13 12:55:18 +0200

    Rename 'logs' to 'logRecords' in OTLP/HTTP

--- src/sumologic-logs-exporter/index.ts
+++ src/sumologic-logs-exporter/index.ts
@@ -273,7 +273,7 @@ export class SumoLogicLogsExporter {
                 name,
                 version,
               },
-              logs,
+              logRecords: logs,
             },
           ],
         },
